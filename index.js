babfa0886147189e6cec5ab28af65c03a5bc5cd2
MICHELLE NASIRISI <wanjikunasirisi1@gmail.com>
2023-12-15 13:37:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264; // Each block is 264 feet long
+  }
+ 
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    return blocks * 264;
+  } 
+ function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; // First 400 feet are free
+    } else if (distance <= 2000) {
+      return (distance - 400) * 0.02; // 2 cents per foot for distance over 400 feet
+    } else if (distance <= 2500) {
+      return 25; // Flat fare for distance over 2000 feet and under 2500 feet
+    } else {
+      return 'cannot travel that far'; // Scuber does not allow rides over 2500 feet
+    }
+  } 
